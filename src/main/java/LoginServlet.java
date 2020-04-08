18e58806f0389271062d0fb8ca9e0a72108b8bd9
jitesh456 <jitesh0dubey@gmail.com>
2020-04-08 15:00:59 +0530

    ADDED:Feature for validating username

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -26,8 +26,13 @@ public class LoginServlet extends HttpServlet {
 
         String actualName = req.getParameter("username");
         String actualPassword = req.getParameter("password");
+        PrintWriter out=resp.getWriter();
+        if(actualName.matches("^[A-Z]{1}[a-z]{2,}$")==false) {
+            out.println("<font color=red>username must start with caps and contain 3 character</font><br>");
+        }
+
         String expectedName=getServletConfig().getInitParameter("name");
-       String expectedPassword=getServletConfig().getInitParameter("password");
+        String expectedPassword=getServletConfig().getInitParameter("password");
 
         if(actualName.equals(expectedName) && actualPassword.equals(expectedPassword)){
             req.setAttribute("username",actualName);
@@ -35,7 +40,6 @@ public class LoginServlet extends HttpServlet {
         }
         else{
             RequestDispatcher dispatcher=getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out=resp.getWriter();
             out.println("<font color=red>Entered username or password is wrong</font>");
             dispatcher.include(req,resp);
         }
